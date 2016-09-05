ff9c77bea8997172177dd9703dcb3c077c19831f
Alexy Stupakov <workinweb24@gmail.com>
2016-09-06 00:08:28 +0300

    Главный сайтбар

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,25 @@
 $(document ).ready(function(){	 
+	 
+	/*
+	 * Select2
+	 */
+
 	 $("#currencies").select2({
 	 	minimumResultsForSearch: -1,
 	 });
 
-});
+	 /*
+	 * owlCarousel
+	 */
+
+	 $("#main-slider").owlCarousel({
+      navigation : true,
+      navigationText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+      singleItem : true, 
+      autoPlay:3000,
+	 });
+
+		});
 
 $(window).scroll(function(){
 
